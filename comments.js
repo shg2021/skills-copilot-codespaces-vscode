e68b642e4bd15043ec2cc166c158e570db8aa1af
shg2021 <89906457+shg2021@users.noreply.github.com>
2023-07-19 08:48:26 +0000

    Copilot Comments

--- comments.js
+++ comments.js
@@ -1,3 +1,54 @@
-// create web serverwith node.js
-// run 'node comments.js' in terminal
-// then open 'http://localhost:3000' in browser
+// create web server
+const express = require('express');
+const app = express();
+
+// parse request body
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// serve static files from public directory
+app.use(express.static('public'));
+
+// database
+const db = require('./db');
+
+// routes
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', { root: __dirname });
+});
+
+// get all comments
+app.get('/api/comments', (req, res) => {
+  db.getAllComments()
+    .then((comments) => {
+      res.send(comments);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Database error');
+    });
+});
+
+// add a comment
+app.post('/api/comments', (req, res) => {
+  const { name, comment } = req.body;
+  if (!name || !comment) {
+    res.status(400).send('Missing name or comment');
+  } else {
+    db.addComment(name, comment)
+      .then(() => {
+        res.status(201).send('Comment added');
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send('Database error');
+      });
+  }
+});
+
+// start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
