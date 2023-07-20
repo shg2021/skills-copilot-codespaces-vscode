ac738b7cee52642eef91e82745b78e03b864c197
shg2021 <89906457+shg2021@users.noreply.github.com>
2023-07-20 05:13:33 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,1 @@
-// create web server with express
-var http = require('http'); // import http module
-var express = require('express'); // import express module
-var app = express(); // create express object
-var server = http.createServer(app); // create server with express object
-var path = require('path'); // import path module
-var bodyParser = require('body-parser'); // import body-parser module
-var mongoose = require('mongoose'); // import mongoose module
-var Comment = require('./models/comment'); // import comment model
-var port = 3000; // set port number
+// Create web server using Express
