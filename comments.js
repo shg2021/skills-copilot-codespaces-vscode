167b3f521ad7a492c34fb9bcb7c2ca7899e8c277
shg2021 <89906457+shg2021@users.noreply.github.com>
2023-07-20 05:08:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -5,7 +5,3 @@ var server = http.createServer(function (req, res) {   // 2 - creating server
 
     //handle incomming requests here..
 });
-
-server.listen(5000); //3 - listen for any incoming requests
-
-console.log('Node.js web server at port 5000 is running..')
